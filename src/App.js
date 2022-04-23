ccb68da9fbf67523268e2d3da0f983edd4709fe4
boyarov.a <boyarov.a@it-universe.ru>
2022-04-23 10:52:45 +0400

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Creator from "./components/Creator";
 import Login from "./components/Login";
+import {Route, Routes} from "react-router";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Creator/>
-      {/*<Login/>*/}
-    </div>
+      <div className="App">
+          <BrowserRouter>
+                <Routes>
+                    <Route path="/api/creator" element={<Creator/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+          </BrowserRouter>
+      </div>
   );
 }
 
