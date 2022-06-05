c5c2cf49b0a0fdca7d8cf2aa12e5a3d700fef014
boyarov.a <boyarov.a@it-universe.ru>
2022-06-05 12:42:50 +0400

    login gives tokens

--- src/components/test/AuthService.js
+++ src/components/test/AuthService.js
@@ -1,12 +1,18 @@
 import axios from "axios";
-const API_URL = "http://localhost:8041/api/auth/";
+const API_URL = "http://localhost:8041/login";
 class AuthService {
     login(username, password) {
+        const params = new URLSearchParams();
+        params.append('username', username);
+        params.append('password', password);
         return axios
-            .post(API_URL + "signin", {
-                username,
-                password
-            })
+            .post(API_URL,
+                params/*,
+                {
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded'
+                    }
+                }*/)
             .then(response => {
                 if (response.data.accessToken) {
                     localStorage.setItem("user", JSON.stringify(response.data));
